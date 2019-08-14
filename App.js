b0a60f7a0f674e1d8600dc8e69afd04ec782de65
Vincent Villaluna <villalunavincent@gmail.com>
2019-08-14 11:02:56 +0800

    updated style

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default class App extends Component {
+  render() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Vindecode</Text>
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 50,
   },
 });
